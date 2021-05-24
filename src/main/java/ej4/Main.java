2aa30e6a638a533925f09334dab60fb4a6cba75b
Daiana <alonsodaiana5@gmail.com>
2021-05-24 13:38:50 -0300

    Inicial

--- src/main/java/ej4/Main.java
+++ src/main/java/ej4/Main.java
@@ -2,8 +2,8 @@ package ej4;
 
 public class Main {
     public static void main(String[] args) {
-       // RestCall rest = new RestCall(
-            //    "https://jsonplaceholder.typicode.com/posts");
-        //System.out.println(rest.run());
+        RestCall rest = new RestCall(
+                "https://jsonplaceholder.typicode.com/posts");
+        System.out.println(rest.run());
     }
 }
